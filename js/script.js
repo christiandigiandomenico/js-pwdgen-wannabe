10b024438c047bbe16bcfd2ea9add706111946c3
christiandigiandomenico <christian.digiandomenico1990@gmail.com>
2024-01-25 13:50:06 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let nome;
+nome = prompt("Come ti chiami?")
+
+let cognome;
+cognome = prompt("Qual è il tuo cognome?")
+
+let colore;
+colore = prompt("Qual è il tuo colore preferito")
+
+document.getElementById("password").innerHTML = nome + cognome + colore + 23
