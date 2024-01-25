60b1461dc9ea94705894defce0d4621f499c6e25
christiandigiandomenico <christian.digiandomenico1990@gmail.com>
2024-01-25 16:21:14 +0100

    Messe piccole aggiunte al codice

--- js/script.js
+++ js/script.js
@@ -9,4 +9,10 @@ colore = prompt("Qual è il tuo colore preferito")
 
 const passcode = nome + cognome + colore + 23
 
-document.getElementById("password").innerHTML = passcode
+document.getElementById("userName").innerHTML = nome
+
+document.getElementById("userSurname").innerHTML = cognome
+
+document.getElementById("userColor").innerHTML = colore
+
+document.getElementById("password").innerHTML = `La tua password è: ${passcode}`
